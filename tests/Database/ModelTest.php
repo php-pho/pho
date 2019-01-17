94023ba9d1eef9468740f550784bdfa801ca97bb
KhanhIceTea <khanhicetea@gmail.com>
2019-01-17 11:24:22 +0700

    merge test

--- tests/Database/ModelTest.php
+++ tests/Database/ModelTest.php
@@ -25,24 +25,18 @@ class ModelTest extends TestCase {
         $model->is_admin = 1;
         $model->deleted_at = $now = new DateTime();
 
+        // getAttributeValue method
+        $this->assertEquals('HELLO', $model->getAttributeValue('key1'));
+        $this->assertEquals(123, $model->getAttributeValue('key2'));
+        $this->assertEquals(true, $model->getAttributeValue('is_admin'));
+        $this->assertEquals($now->format('U'), $model->getAttributeValue('deleted_at')->format('U'));
+
+        // getAttribute method
         $this->assertEquals(null, $model->getAttribute(null));
         $this->assertEquals(null, $model->getAttribute('getAttribute'));
         $this->assertEquals('HELLO', $model->getAttribute('key1'));
         $this->assertEquals(123, $model->getAttribute('key2'));
         $this->assertEquals(true, $model->getAttribute('is_admin'));
         $this->assertEquals($now->format('U'), $model->getAttribute('deleted_at')->format('U'));
-    }
-
-    public function testGetAttributeValue() {
-        $model = new DumbModel();
-        $model->key1 = 'hello';
-        $model->key2 = 123;
-        $model->is_admin = 1;
-        $model->deleted_at = $now = new DateTime();
-
-        $this->assertEquals('HELLO', $model->getAttributeValue('key1'));
-        $this->assertEquals(123, $model->getAttributeValue('key2'));
-        $this->assertEquals(true, $model->getAttributeValue('is_admin'));
-        $this->assertEquals($now->format('U'), $model->getAttributeValue('deleted_at')->format('U'));
     }
 }
